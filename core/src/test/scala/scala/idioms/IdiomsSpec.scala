915032c6efcbee3ae1ccedadc94e50d8c86344e7
Evgeny Kotelnikov <evgeny.kotelnikov@gmail.com>
2013-03-13 00:48:26 +0400

    Rename variables in the spec nicely

--- core/src/test/scala/scala/idioms/IdiomsSpec.scala
+++ core/src/test/scala/scala/idioms/IdiomsSpec.scala
@@ -6,11 +6,11 @@ import org.scalatest.matchers.ShouldMatchers
 class IdiomsSpec extends FlatSpec with ShouldMatchers {
   behavior of "Idiom"
 
-  val foo: Option[Int] = Some(10)
-  val bar: Option[Int] = Some(3)
-  val baz: Option[Int] = Some(6)
-  val xyz: Option[Int] = Some(4)
-  val qux: Option[Int] = None
+  val ten:   Option[Int] = Some(10)
+  val three: Option[Int] = Some(3)
+  val six:   Option[Int] = Some(6)
+  val four:  Option[Int] = Some(4)
+  val none:  Option[Int] = None
 
   it should "lift pure expression" in {
     $("foobar") should equal (Some("foobar"))
@@ -18,92 +18,92 @@ class IdiomsSpec extends FlatSpec with ShouldMatchers {
   }
 
   it should "lift object operator application" in {
-    $(foo - bar) should equal (Some(7))
-    $(bar - qux) should equal (None)
+    $(ten - three)  should equal (Some(7))
+    $(three - none) should equal (None)
   }
 
   it should "lift method application" in {
     def minus(a: Int, b: Int) = a - b
-    $(minus(foo, bar)) should equal (Some(7))
-    $(minus(bar, qux)) should equal (None)
+    $(minus(ten, three))  should equal (Some(7))
+    $(minus(three, none)) should equal (None)
   }
 
   it should "lift curried method application" in {
     def minus(a: Int)(b: Int) = a - b
-    $(minus(foo)(bar)) should equal (Some(7))
-    $(minus(bar)(qux)) should equal (None)
+    $(minus(ten)(three))  should equal (Some(7))
+    $(minus(three)(none)) should equal (None)
   }
 
   it should "lift function application" in {
     val minus = (a: Int, b: Int) => a - b
-    $(minus(foo, bar)) should equal (Some(7))
-    $(minus(bar, qux)) should equal (None)
+    $(minus(ten, three))  should equal (Some(7))
+    $(minus(three, none)) should equal (None)
   }
 
   it should "lift curried function application" in {
     val minus = (a: Int) => (b: Int) => a - b
-    $(minus(foo)(bar)) should equal (Some(7))
-    $(minus(bar)(qux)) should equal (None)
+    $(minus(ten)(three))  should equal (Some(7))
+    $(minus(three)(none)) should equal (None)
   }
 
   it should "lift partially lifted object operator application" in {
-    $(foo - 2)  should equal (Some(8))
-    $(qux - 2)  should equal (None)
-    $(14 - foo) should equal (Some(4))
-    $(14 - qux) should equal (None)
+    $(ten - 2)   should equal (Some(8))
+    $(none - 2)  should equal (None)
+    $(14 - ten)  should equal (Some(4))
+    $(14 - none) should equal (None)
   }
 
   it should "lift partially lifted method application" in {
     def minus(a: Int, b: Int) = a - b
-    $(minus(foo, 2))  should equal (Some(8))
-    $(minus(qux, 2))  should equal (None)
-    $(minus(12, foo)) should equal (Some(2))
-    $(minus(12, qux)) should equal (None)
+    $(minus(ten, 2))   should equal (Some(8))
+    $(minus(none, 2))  should equal (None)
+    $(minus(12, ten))  should equal (Some(2))
+    $(minus(12, none)) should equal (None)
   }
 
   it should "lift partially lifted curried method application" in {
     def minus(a: Int)(b: Int) = a - b
-    $(minus(foo)(2))  should equal (Some(8))
-    $(minus(qux)(2))  should equal (None)
-    $(minus(12)(foo)) should equal (Some(2))
-    $(minus(12)(qux)) should equal (None)
+    $(minus(ten)(2))   should equal (Some(8))
+    $(minus(none)(2))  should equal (None)
+    $(minus(12)(ten))  should equal (Some(2))
+    $(minus(12)(none)) should equal (None)
   }
 
   it should "lift partially lifted function application" in {
     val minus = (a: Int, b: Int) => a - b
-    $(minus(foo, 2))  should equal (Some(8))
-    $(minus(qux, 2))  should equal (None)
-    $(minus(12, foo)) should equal (Some(2))
-    $(minus(12, qux)) should equal (None)
+    $(minus(ten, 2))   should equal (Some(8))
+    $(minus(none, 2))  should equal (None)
+    $(minus(12, ten))  should equal (Some(2))
+    $(minus(12, none)) should equal (None)
   }
 
   it should "lift partially lifted curried function application" in {
     val minus = (a: Int) => (b: Int) => a - b
-    $(minus(foo)(2))  should equal (Some(8))
-    $(minus(qux)(2))  should equal (None)
-    $(minus(12)(foo)) should equal (Some(2))
-    $(minus(12)(qux)) should equal (None)
+    $(minus(ten)(2))   should equal (Some(8))
+    $(minus(none)(2))  should equal (None)
+    $(minus(12)(ten))  should equal (Some(2))
+    $(minus(12)(none)) should equal (None)
   }
 
   it should "lift compound funcall with all the arguments lifted" in {
-    $(foo - (baz / bar)) should equal (Some(8))
-    $(foo - (qux / bar)) should equal (None)
-    $((foo - xyz) / bar) should equal (Some(2))
-    $((foo - qux) / bar) should equal (None)
+    $(ten - (six / three))  should equal (Some(8))
+    $(ten - (none / three)) should equal (None)
+    $((ten - four) / three) should equal (Some(2))
+    $((ten - none) / three) should equal (None)
   }
 
   it should "lift compound funcall with some of the arguments non-lifted" in {
-    $(foo - (baz / 2))  should equal (Some(7))
-    $(foo - (qux / 2))  should equal (None)
-    $(foo - (18 / baz)) should equal (Some(7))
-    $(foo - (18 / qux)) should equal (None)
-    $(2 * (foo - baz))  should equal (Some(8))
-    $(2 * (foo - qux))  should equal (None)
-    $((foo - baz) * 3)  should equal (Some(12))
-    $((foo - qux) * 3)  should equal (None)
-    $((foo - 4) / baz)  should equal (Some(1))
-    $((foo - 4) / qux)  should equal (None)
-    $((1 + foo) * baz)  should equal (Some(66))
-    $((1 + foo) * qux)  should equal (None)
+    $(ten - (six / 2))   should equal (Some(7))
+    $(ten - (none / 2))  should equal (None)
+    $(ten - (18 / six))  should equal (Some(7))
+    $(ten - (18 / none)) should equal (None)
+    $(2 * (ten - six))   should equal (Some(8))
+    $(2 * (ten - none))  should equal (None)
+    $((ten - six) * 3)   should equal (Some(12))
+    $((ten - none) * 3)  should equal (None)
+    $((ten - 4) / six)   should equal (Some(1))
+    $((ten - 4) / none)  should equal (None)
+    $((1 + ten) * six)   should equal (Some(66))
+    $((1 + ten) * none)  should equal (None)
   }
 }
