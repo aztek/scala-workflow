856d85970dab4957049b256c3a0443a8c28e0102
Evgeny Kotelnikov <evgeny.kotelnikov@gmail.com>
2013-03-17 19:55:37 +0400

    Provide instance of applicative functor for functions and util.Try

--- core/src/main/scala/scala/idioms/Applicative.scala
+++ core/src/main/scala/scala/idioms/Applicative.scala
@@ -1,6 +1,7 @@
 package scala.idioms
 
 import language.higherKinds
+import util.Try
 
 trait Applicative[F[_]] {
   def pure[T](t: T): F[T]
@@ -16,6 +17,19 @@ object Applicative {
   implicit val list = new Applicative[List] {
     def pure[A](a: A) = List(a)
     def app[A, B](fs: List[A ⇒ B]) = as ⇒ for (f ← fs; a ← as) yield f(a)
+  }
+
+  implicit val try_ = new Applicative[Try] {
+    def pure[A](a: A) = Try(a)
+    def app[A, B](ff: Try[A ⇒ B]) = aa ⇒ for (f ← ff; a ← aa) yield f(a)
+  }
+
+  implicit def function[T] = {
+    type `T ⇒ _`[A] = T ⇒ A
+    new Applicative[`T ⇒ _`] {
+      def pure[A](a: A) = (_: T) ⇒ a
+      def app[A, B](f: T ⇒ A ⇒ B) = (g: T ⇒ A) ⇒ (t: T) ⇒ f(t)(g(t))
+    }
   }
 
   def pure[F[_] : Applicative, A](a: A) = implicitly[Applicative[F]].pure[A](a)
