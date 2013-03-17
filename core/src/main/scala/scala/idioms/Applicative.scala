216843b60e027a6f2180b1b0216bf83e57744fca
Evgeny Kotelnikov <evgeny.kotelnikov@gmail.com>
2013-03-17 20:00:29 +0400

    Pass applicative pure arguments lazily

--- core/src/main/scala/scala/idioms/Applicative.scala
+++ core/src/main/scala/scala/idioms/Applicative.scala
@@ -4,35 +4,35 @@ import language.higherKinds
 import util.Try
 
 trait Applicative[F[_]] {
-  def pure[T](t: T): F[T]
+  def pure[T](t: ⇒ T): F[T]
   def app[A, B](f: F[A ⇒ B]): F[A] ⇒ F[B]
 }
 
 object Applicative {
   implicit val option = new Applicative[Option] {
-    def pure[A](a: A) = Some(a)
+    def pure[A](a: ⇒ A) = Some(a)
     def app[A, B](optF: Option[A ⇒ B]) = optA ⇒ for (f ← optF; a ← optA) yield f(a)
   }
 
   implicit val list = new Applicative[List] {
-    def pure[A](a: A) = List(a)
+    def pure[A](a: ⇒ A) = List(a)
     def app[A, B](fs: List[A ⇒ B]) = as ⇒ for (f ← fs; a ← as) yield f(a)
   }
 
   implicit val try_ = new Applicative[Try] {
-    def pure[A](a: A) = Try(a)
+    def pure[A](a: ⇒ A) = Try(a)
     def app[A, B](ff: Try[A ⇒ B]) = aa ⇒ for (f ← ff; a ← aa) yield f(a)
   }
 
   implicit def function[T] = {
     type `T ⇒ _`[A] = T ⇒ A
     new Applicative[`T ⇒ _`] {
-      def pure[A](a: A) = (_: T) ⇒ a
+      def pure[A](a: ⇒ A) = (_: T) ⇒ a
       def app[A, B](f: T ⇒ A ⇒ B) = (g: T ⇒ A) ⇒ (t: T) ⇒ f(t)(g(t))
     }
   }
 
-  def pure[F[_] : Applicative, A](a: A) = implicitly[Applicative[F]].pure[A](a)
+  def pure[F[_] : Applicative, A](a: ⇒ A) = implicitly[Applicative[F]].pure[A](a)
 
-  def app[F[_] : Applicative, A, B](f: F[A ⇒ B])(a: F[A]) = implicitly[Applicative[F]].app(f)(a)
+  def app[F[_] : Applicative, A, B](f: F[A ⇒ B])(a: ⇒ F[A]) = implicitly[Applicative[F]].app(f)(a)
 }
