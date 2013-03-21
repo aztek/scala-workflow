dae1c7883de0c4c0477b6b56b24114bf6787aa45
Evgeny Kotelnikov <evgeny.kotelnikov@gmail.com>
2013-03-21 12:39:11 +0400

    Remove Array implementation (it's actually not obvious), add applicatives for functions

--- core/src/main/scala/scala/idioms/Applicative.scala
+++ core/src/main/scala/scala/idioms/Applicative.scala
@@ -24,11 +24,6 @@ object Applicative {
     def app[A, B](fs: Set[A ⇒ B]) = as ⇒ for (f ← fs; a ← as) yield f(a)
   }
 
-  implicit val array = new Applicative[Array] {
-    def pure[A](a: ⇒ A) = Array(a)
-    def app[A, B](fs: Array[A ⇒ B]) = as ⇒ for (f ← fs; a ← as) yield f(a)
-  }
-
   implicit val try_ = new Applicative[Try] {
     def pure[A](a: ⇒ A) = Try(a)
     def app[A, B](ff: Try[A ⇒ B]) = aa ⇒ for (f ← ff; a ← aa) yield f(a)
@@ -50,11 +45,51 @@ object Applicative {
     }
   }
 
-  implicit def function[T] = {
-    type `T ⇒ _`[A] = T ⇒ A
-    new Applicative[`T ⇒ _`] {
-      def pure[A](a: ⇒ A) = (_: T) ⇒ a
-      def app[A, B](f: T ⇒ A ⇒ B) = g ⇒ t ⇒ f(t)(g(t))
+  implicit def function1[R] = {
+    type `R ⇒ _`[A] = R ⇒ A
+    new Applicative[`R ⇒ _`] {
+      def pure[A](a: ⇒ A) = (_: R) ⇒ a
+      def app[A, B](f: R ⇒ A ⇒ B) = g ⇒ t ⇒ f(t)(g(t))
+    }
+  }
+
+  implicit def function2[R, S] = {
+    type `R ⇒ S ⇒ _`[A] = R ⇒ S ⇒ A
+    new Applicative[`R ⇒ S ⇒ _`] {
+      def pure[A](a: ⇒ A) = (_: R) ⇒ (_: S) ⇒ a
+      def app[A, B](f: R ⇒ S ⇒ A ⇒ B) = g ⇒ r ⇒ s ⇒ f(r)(s)(g(r)(s))
+    }
+  }
+
+  implicit def function3[R, S, T] = {
+    type `R ⇒ S ⇒ T ⇒ _`[A] = R ⇒ S ⇒ T ⇒ A
+    new Applicative[`R ⇒ S ⇒ T ⇒ _`] {
+      def pure[A](a: ⇒ A) = (_: R) ⇒ (_: S) ⇒ (_: T) ⇒ a
+      def app[A, B](f: R ⇒ S ⇒ T ⇒ A ⇒ B) = g ⇒ r ⇒ s ⇒ t ⇒ f(r)(s)(t)(g(r)(s)(t))
+    }
+  }
+
+  implicit def function4[R, S, T, U] = {
+    type `R ⇒ S ⇒ T ⇒ U ⇒ _`[A] = R ⇒ S ⇒ T ⇒ U ⇒ A
+    new Applicative[`R ⇒ S ⇒ T ⇒ U ⇒ _`] {
+      def pure[A](a: ⇒ A) = (_: R) ⇒ (_: S) ⇒ (_: T) ⇒ (_: U) ⇒ a
+      def app[A, B](f: R ⇒ S ⇒ T ⇒ U ⇒ A ⇒ B) = g ⇒ r ⇒ s ⇒ t ⇒ u ⇒ f(r)(s)(t)(u)(g(r)(s)(t)(u))
+    }
+  }
+
+  implicit def function5[R, S, T, U, V] = {
+    type `R ⇒ S ⇒ T ⇒ U ⇒ V ⇒ _`[A] = R ⇒ S ⇒ T ⇒ U ⇒ V ⇒ A
+    new Applicative[`R ⇒ S ⇒ T ⇒ U ⇒ V ⇒ _`] {
+      def pure[A](a: ⇒ A) = (_: R) ⇒ (_: S) ⇒ (_: T) ⇒ (_: U) ⇒ (_: V) ⇒ a
+      def app[A, B](f: R ⇒ S ⇒ T ⇒ U ⇒ V ⇒ A ⇒ B) = g ⇒ r ⇒ s ⇒ t ⇒ u ⇒ v ⇒ f(r)(s)(t)(u)(v)(g(r)(s)(t)(u)(v))
+    }
+  }
+
+  implicit def function6[R, S, T, U, V, W] = {
+    type `R ⇒ S ⇒ T ⇒ U ⇒ V ⇒ W ⇒ _`[A] = R ⇒ S ⇒ T ⇒ U ⇒ V ⇒ W ⇒ A
+    new Applicative[`R ⇒ S ⇒ T ⇒ U ⇒ V ⇒ W ⇒ _`] {
+      def pure[A](a: ⇒ A) = (_: R) ⇒ (_: S) ⇒ (_: T) ⇒ (_: U) ⇒ (_: V) ⇒ (_: W) ⇒ a
+      def app[A, B](f: R ⇒ S ⇒ T ⇒ U ⇒ V ⇒ W ⇒ A ⇒ B) = g ⇒ r ⇒ s ⇒ t ⇒ u ⇒ v ⇒ w ⇒ f(r)(s)(t)(u)(v)(w)(g(r)(s)(t)(u)(v)(w))
     }
   }
 }
