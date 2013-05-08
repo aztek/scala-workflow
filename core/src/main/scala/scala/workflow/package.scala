4fad303976c7ff67eb29b8c6841ce48b6c795ce8
Evgeny Kotelnikov <evgeny.kotelnikov@gmail.com>
2013-05-08 14:23:59 +0400

    Debug resolveLiftedType and typeCheck methods

--- core/src/main/scala/scala/workflow/package.scala
+++ core/src/main/scala/scala/workflow/package.scala
@@ -122,49 +122,42 @@ package object workflow extends FunctorInstances with SemiIdiomInstances with Mo
       produceApplication(lambda)(args)
     }
 
-    def resolveLiftedType(tpe: Type): Type = {
-      val TypeRef(_, _, typeArgs) = tpe
-      idiom match {
-        case PolyType(List(wildcard), typeRef: TypeRef) ⇒
-          // When idiom is passed as type lambda, we need to take the type
-          // from wildcard position, so we zip through both typerefs to seek for a substitution
-          def findSubstitution(wildcardedType: Type, concreteType: Type): Option[Type] = {
-            if (wildcardedType.typeSymbol == wildcard)
-              Some(concreteType)
-            else
-              (wildcardedType, concreteType) match {
-                case (wctpe: TypeRef, ctpe: TypeRef) ⇒
-                  wctpe.args zip ctpe.args map {
-                    case (wct, at) ⇒ findSubstitution(wct, at)
-                  } collectFirst {
-                    case Some(t) ⇒ t
+    def resolveLiftedType(tpe: Type): Option[Type] =
+      tpe.baseType(idiom.typeSymbol) match {
+        case baseType @ TypeRef(_, _, typeArgs) ⇒
+          idiom match {
+            case PolyType(List(wildcard), typeRef: TypeRef) ⇒
+              // When idiom is passed as type lambda, we need to take the type
+              // from wildcard position, so we zip through both typerefs to seek for a substitution
+              def findSubstitution(wildcardedType: Type, concreteType: Type): Option[Type] = {
+                if (wildcardedType.typeSymbol == wildcard)
+                  Some(concreteType)
+                else
+                  (wildcardedType, concreteType) match {
+                    case (wctpe: TypeRef, ctpe: TypeRef) ⇒
+                      wctpe.args zip ctpe.args find {
+                        case (wct, at) ⇒ !(wct =:= at)
+                      } flatMap {
+                        case (wct, at) ⇒ findSubstitution(wct, at)
+                      }
+                    case _ ⇒ None
                   }
-                case _ ⇒ None
               }
+              findSubstitution(typeRef, baseType)
+            case _ ⇒
+              // This only works for type constructor of one argument
+              // TODO: provide implementation for n-arity type constructors
+              typeArgs.headOption
           }
-          findSubstitution(typeRef, tpe) getOrElse {
-            c.abort(c.enclosingPosition, "Unable to lift argument to an idiom")
-          }
-        case _ ⇒
-          // This only works for type constructor of one argument
-          // TODO: provide implementation for n-arity type constructors
-          typeArgs.head
+        case _ ⇒ None
       }
-    }
 
     def typeCheck(tree: Tree, binds: Binds): Option[Tree] = {
       val vals = binds map { case (name, tpe, _) ⇒ q"val $name : ${TypeTree(tpe).duplicate} = ???" }
       try {
         Some(c.typeCheck(q"{ ..$vals; ${tree.duplicate} }"))
       } catch {
-        case e: TypecheckException if e.msg contains "follow this method with `_'" ⇒
-          try {
-            val newvals = binds map { case (name, tpe, _) ⇒ q"val $name : ${TypeTree(tpe).duplicate} = ???" }
-            Some(c.typeCheck(q"{ ..$newvals; ${tree.duplicate} _ }"))
-          } catch {
-            case e: TypecheckException if e.msg contains "ambiguous reference" ⇒ Some(EmptyTree)
-            case e: Exception ⇒ None
-          }
+        case e: TypecheckException if e.msg contains "follow this method with `_'" ⇒ Some(EmptyTree)
         case e: TypecheckException if e.msg contains "missing arguments for constructor" ⇒
           try {
             val newvals = binds map { case (name, tpe, _) ⇒ q"val $name : ${TypeTree(tpe).duplicate} = ???" }
@@ -174,11 +167,10 @@ package object workflow extends FunctorInstances with SemiIdiomInstances with Mo
             case e: Exception ⇒ None
           }
         case e: TypecheckException if e.msg contains "ambiguous reference" ⇒ Some(EmptyTree)
+        case e: TypecheckException if (e.msg contains "package") && (e.msg contains "is not a value") ⇒ Some(EmptyTree)
         case e: Exception ⇒ None
       }
     }
-
-    def isLifted(tpe: Type) = tpe.baseClasses contains idiom.typeSymbol
 
     type Bind  = (TermName, Type, Tree)
     type Binds = List[Bind]
@@ -196,12 +188,12 @@ package object workflow extends FunctorInstances with SemiIdiomInstances with Mo
 
     def rewrite(binds: Binds): Tree ⇒ (Tree, Binds) = {
       case Apply(fun, args) ⇒
-        val (newfun,  funbinds)  = extractLambdaBody(binds)(fun)
-        val (newargs, argsbinds) = args.map(extractLambdaBody(funbinds)).unzip
+        val (newfun,  funbinds)  = rewrite(binds)(fun)
+        val (newargs, argsbinds) = args.map(rewrite(funbinds)).unzip
         extractLambdaBody(argsbinds.flatten.distinct)(q"$newfun(..$newargs)")
 
       case Select(value, method) ⇒
-        val (newvalue, newbinds) = extractLambdaBody(binds)(value)
+        val (newvalue, newbinds) = rewrite(binds)(value)
         extractLambdaBody(newbinds)(q"$newvalue.$method")
 
       case value @ Literal(_) ⇒
@@ -210,23 +202,27 @@ package object workflow extends FunctorInstances with SemiIdiomInstances with Mo
       case ident @ Ident(_) ⇒
         extractLambdaBody(binds)(ident)
 
+      case constructor @ New(_) ⇒
+        extractLambdaBody(binds)(constructor)
+
       case expr ⇒
         c.abort(expr.pos, "Unsupported expression " + showRaw(expr))
     }
 
     def extractLambdaBody(binds: Binds): Tree ⇒ (Tree, Binds) = expr ⇒ {
       typeCheck(expr, binds) match {
-        case None ⇒
-          rewrite(binds)(expr)
+        case None ⇒ rewrite(binds)(expr)
 
-        case Some(typeTree) if isLifted(typeTree.tpe) ⇒
-          val name = TermName(c.freshName("arg$"))
-          val tpe  = resolveLiftedType(typeTree.tpe)
-          val bind = (name, tpe, expr)
-          (q"$name", bind :: binds)
+        case Some(typeTree) ⇒
+          resolveLiftedType(typeTree.tpe) match {
+            case Some(tpe) ⇒
+              val name = TermName(c.freshName("arg$"))
+              val bind = (name, tpe, expr)
+              (q"$name", bind :: binds)
 
-        case _ ⇒
-          (expr, binds)
+            case None ⇒
+              (expr, binds)
+          }
       }
     }
 
