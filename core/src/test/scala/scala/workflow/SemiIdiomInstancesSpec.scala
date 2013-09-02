42f2be078e42bb5e46d1761f03aedcb7b7112912
aztek <evgeny.kotelnikov@gmail.com>
2013-09-02 11:17:40 +0400

    Fix typo

--- core/src/test/scala/scala/workflow/SemiIdiomInstancesSpec.scala
+++ core/src/test/scala/scala/workflow/SemiIdiomInstancesSpec.scala
@@ -4,7 +4,7 @@ import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
 
 class SemiIdiomInstancesSpec extends FlatSpec with ShouldMatchers {
-  behavior of "Built-in semi-context instances"
+  behavior of "Built-in semi-idiom instances"
 
   "ZipLists" should "work" in {
     context(zipList) {
